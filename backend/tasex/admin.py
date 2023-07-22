1c51c706406bf76d4fa1f40844a0b97265ff8acf
ibnabii <someone@somewhere.net>
2023-07-22 09:15:10 +0200

    feat: added Product model

--- backend/tasex/admin.py
+++ backend/tasex/admin.py
@@ -1,12 +1,29 @@
 from django.contrib import admin
 
-from .models import Experiment, Panel
+from .models import Experiment, Panel, Product
 
 
 @admin.register(Experiment)
 class ExperimentAdmin(admin.ModelAdmin):
     readonly_fields = ('created_at', )
-    list_display = ('title', 'created_at', 'description',)
+    list_display = ('internal_title', 'title', 'created_at', 'prod_a_internal_name', 'prod_b_internal_name')
+    ordering = ('-created_at', )
+
+    def prod_a_internal_name(self, obj):
+        return obj.product_A.internal_name
+    prod_a_internal_name.short_description = 'Product A'
+
+    def prod_b_internal_name(self, obj):
+        return obj.product_B.internal_name
+
+    prod_b_internal_name.short_description = 'Product B'
+
+    def get_queryset(self, request):
+        return super(ExperimentAdmin, self).get_queryset(request).select_related('product_A', 'product_B')
+
+    # alternatively one could use (?)
+    # class ArticleAdmin(admin.ModelAdmin):
+    #     list_select_related = ["author", "category"]
 
 
 @admin.register(Panel)
@@ -33,3 +50,12 @@ class PanelAdmin(admin.ModelAdmin):
         'created_at',
     )
 
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'brew_id',
+        'internal_name',
+        'name',
+        'description',
+    )
--- backend/tasex/models.py
+++ backend/tasex/models.py
@@ -3,11 +3,24 @@ from uuid import uuid4
 from django.db import models
 
 
+class Product(models.Model):
+    brew_id = models.CharField(max_length=20, help_text='Internal brewing ID, not shown to panelists')
+    internal_name = models.CharField(max_length=100, help_text='Internal name, not shown to panelists')
+    name = models.CharField(max_length=100, help_text='Name of the product shown to panelists')
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
+
+
 class Experiment(models.Model):
     id = models.UUIDField(primary_key=True, editable=False, default=uuid4)
-    title = models.CharField(max_length=120)
-    description = models.TextField()
+    internal_title = models.CharField(max_length=120, help_text='Internal title, not shown to panelists')
+    title = models.CharField(max_length=120, help_text='Title shown to panelists')
+    description = models.TextField(help_text='Decide if show this description to panelists on panel level')
     created_at = models.DateTimeField(auto_now_add=True)
+    product_A = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='+')
+    product_B = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='+')
 
     def __str__(self):
         return self.title
@@ -18,6 +31,16 @@ class Panel(models.Model):
     experiment = models.ForeignKey(Experiment, on_delete=models.CASCADE, related_name='panels')
     description = models.TextField()
     planned_samples = models.PositiveSmallIntegerField()
+    class ShowExperimentDescription(models.TextChoices):
+        NO = 'NO', 'No'
+        AFTER_TASTING= 'AFTER_TASTING', 'Show after tasting',
+        BEFORE_TASTING = 'BEFORE_TASTING', 'Show before tasting'
+    show_exp_description = models.CharField(
+        choices=ShowExperimentDescription.choices,
+        default=ShowExperimentDescription.NO,
+        max_length=20,
+        verbose_name="Show experiment description to panelists?"
+    )
     is_active = models.BooleanField(default=True, help_text='if Panel accepts results')
     closed_at = models.DateTimeField(null=True, blank=True, editable=False)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -25,3 +48,14 @@ class Panel(models.Model):
 
     def __str__(self):
         return self.description[:50]
+
+
+class SampleSet(models.Model):
+    panel = models.ForeignKey(Panel, on_delete=models.CASCADE, related_name='sample_sets')
+    is_used = models.BooleanField(default=False)
+
+
+class Sample(models.Model):
+    sample_set = models.ForeignKey(SampleSet, on_delete=models.CASCADE, related_name='samples')
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='samples')
+    code = models.CharField(max_length=5)
