c3444ccc8816641b2e4925e1142feb8b998cb1b1
ibnabii <noone@nowhere.org>
2023-07-28 17:15:37 +0200

    feat: QR generation for link to panel

--- backend/tasex/urls.py
+++ backend/tasex/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from .views import AdminPanelView
+from .views import AdminPanelView, render_qr_code
 
 app_name = 'tasex'
 
 urlpatterns = [
     path('<pk>/', AdminPanelView.as_view(), name='panel'),
+    path('<pk>/qr/', render_qr_code, name='panel-qr'),
 ]
--- backend/tasex/views.py
+++ backend/tasex/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
+import qrcode
+
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.http import HttpResponse
+from django.shortcuts import reverse
 from django.views.generic import DetailView
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets, permissions
@@ -26,3 +29,22 @@ class AdminPanelView(LoginRequiredMixin, DetailView):
     template_name = 'tasex/admin_panel.html'
     model = Panel
     queryset = Panel.objects.filter(is_active=True)
+
+
+def render_qr_code(request, pk):
+
+    qr = qrcode.QRCode(
+        version=None,
+        error_correction=qrcode.constants.ERROR_CORRECT_Q,
+        box_size=5,
+        border=4,
+    )
+    qr.add_data(
+        request.build_absolute_uri(reverse('tasex:panel', kwargs={'pk': pk}))
+    )
+    qr.make(fit=True)
+
+    img = qr.make_image(fill_color="black", back_color="white")
+    response = HttpResponse(content_type="image/png")
+    img.save(response, 'PNG')
+    return response
