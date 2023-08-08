f5a585df95a9b87e71f10b75274d7c29ddd91e8f
ibnabii <someone@somewhere.net>
2023-08-08 18:46:37 +0200

    fix: added CSRF_TRUSTED_ORIGINS config

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -20,7 +20,7 @@ SECRET_KEY = env("SECRET_KEY", default=get_random_secret_key())
 DEBUG = env("DEBUG", default=False)
 
 ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=['127.0.0.1'])
-
+CSRF_TRUSTED_ORIGINS = env.list("CSRF_TRUSTED_ORIGINS", default=['127.0.0.1'])
 
 # Application definition
 
